92977fbc9bb61df95e25c11b7f613d79bf50e8ed
JorgeRuedac <rueda6991@hotmail.com>
2019-01-30 14:44:57 -0500

    Agregar archivos de proyecto.

---
+++ olimp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace olimp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
